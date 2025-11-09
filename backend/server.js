03eb6e03b3fe1eeeb079c8194a663040309f17af
maniganta27-source <maniganta27@gmail.com>
2025-11-09 11:20:36 +0530

    Updated Server

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const path = require("path");
+app.use(express.static(path.join(__dirname, "frontend", "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
+
 // Routes
 app.use("/api/auth", authRoutes);
 
