0b9a1fb9b43a9997f6dda4da9c7c004664c32abe
maniganta27-source <maniganta27@gmail.com>
2025-11-09 11:40:22 +0530

    server Updated

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import dotenv from "dotenv";
 import express from "express";
-
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 
@@ -16,7 +18,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const path = require("path");
+// ES module workaround for __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve frontend
 app.use(express.static(path.join(__dirname, "frontend", "build")));
 
 app.get("*", (req, res) => {
